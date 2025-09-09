53aa1a5e911f5122d2598cab1c8a36b01c9820e8
Gohil Gnandipainh <abhijitsinh438@gmail.com>
2025-09-09 16:37:43 +0530

    Delete python/07_python_lab work/lab_work_6 directory

--- python/07_python_lab work/lab_work_6/Q1.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-
-
-print(f"Hello, {last_name}, {first_name}!")
--- python/07_python_lab work/lab_work_6/Q2.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-item = "apple"
-price = 5.50
-
-
-print(f"The price of {item} is {price} dollars.")
